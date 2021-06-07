1d1acb29b32f56a7338ad18c7dacd0c81e655110
zyr <zyr1998527@126.com>
2021-06-07 18:42:22 +0800

    update the procedure of processing data

--- process_data/utils.py
+++ process_data/utils.py
@@ -9,3 +9,10 @@ def read_file(filename):
     with open(filename, "r", encoding="utf-8") as fin:
         return fin.readlines()
 
+
+def write_file(sents, filename):
+    print("Writing data to {}...".format(filename))
+    with open(filename, "w", encoding="utf-8") as fout:
+        for sent in sents:
+            fout.write(sent)
+
